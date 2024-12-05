da18a1cbad8779ed9654188dc98fbc23a30c4c24
Vaibhavlll <vaibhavp0509@gmail.com>
2024-12-05 18:13:43 +0530

    first commit

---
+++ Frontend/src/home/Leftpart/User.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useConversation from "../../statemanage/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+
+function User({ user }) {
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  const isSelected = selectedConversation?._id === user._id;
+  const { socket, onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(user._id);
+  return (
+    <div
+      className={`hover:bg-slate-600 duration-300 ${
+        isSelected ? "bg-slate-700" : ""
+      }`}
+      onClick={() => setSelectedConversation(user)}
+    >
+      <div className="flex space-x-4 px-8 py-3 hover:bg-slate-700 duration-300 cursor-pointer">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
+          <div className="w-12 rounded-full">
+             <img src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" />
+          </div>
+        </div>
+        <div>
+          <h1 className=" font-bold">{user.name}</h1>
+          <span>{user.fullname}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default User;
---
+++ Frontend/src/home/Rightpart/Chatuser.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import useConversation from "../../statemanage/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+import { CiMenuFries } from "react-icons/ci";
+
+function Chatuser() {
+  const { selectedConversation } = useConversation();
+  const { onlineUsers } = useSocketContext();
+  const getOnlineUsersStatus = (userId) => {
+    return onlineUsers.includes(userId) ? "Online" : "Offline";
+  };
+
+  return (
+    <div className=" pl-5 pt-5 h-[12vh] flex space-x-4 bg-gray-700 hover:bg-gray-600 duration-300">
+      <div>
+        <div className="avatar online">
+          <div className="w-14 rounded-full">
+            <img src="https://t4.ftcdn.net/jpg/02/29/75/83/360_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-xl">{selectedConversation.name}</h1>
+        <span className="text-sm">
+          {getOnlineUsersStatus(selectedConversation._id)}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default Chatuser;
